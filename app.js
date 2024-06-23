b971082dac46fb7eec7d56c07616e1d6c237f59f
joseignacio-ing-agricola <farias.l.jose95@gmail.com>
2024-06-23 19:29:14 -0400

    Juego Del Número Secreto
    juego adivina el numero oracle

---
+++ app.js
@@ -0,0 +1,79 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+
+    // Si ya se han sorteado todos los números
+    if (listaNumerosSorteados.length === numeroMaximo) {
+        asignarTextoElemento('p', 'Todos los números posibles ya se han sorteado');
+        document.getElementById('reiniciar').setAttribute('disabled', 'true');
+        return null;
+    }
+
+    // Si el número generado está incluido en la lista
+    if (listaNumerosSorteados.includes(numeroGenerado)) {
+        return generarNumeroSecreto(); // Llamada recursiva
+    } else {
+        listaNumerosSorteados.push(numeroGenerado); // Agregar el número a la lista
+        return numeroGenerado;
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto!');
+    asignarTextoElemento('p', `Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    if (numeroSecreto === null) {
+        // Si se han sorteado todos los números, no se genera un nuevo número secreto
+        return;
+    }
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    //Reiniciar la lista de números sorteados
+    listaNumerosSorteados = [];
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
+
+condicionesIniciales();
